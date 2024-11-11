1722b34908706db07da803d3d2692989862e7047
Naseem Ansari <mrnaseem745@gmail.com>
2024-11-11 12:16:20 +0530

    #working on gsap animation

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { project, section } from '../../lumenContent';
 import FeaturedCard from './FeaturedCard';
 import Button from '../Button/Button';
-
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 const Featured = () => {
+    const gsapContainerRef = useRef(null);
   return (
-    <section  className='py-[10vw] px-[15vw]'>
-        <div>
+    <section  className='py-[10vw] px-[15vw]' ref={gsapContainerRef}>
+        <div className='gsapContainer'>
             <h1 className='text-2xl max-sm:text-[16px]'>{section.featured.title}</h1>
             <h1 className='featuredTitle w-[50%] max-sm:w-full max-sm:text-3xl'>{section.featured.description}</h1>
         </div>
